f7b540ad303567444629d882783cf606ef75bf8a
Harvey Wu <hwca96@gmail.com>
2023-06-02 13:47:10 -0700

    basic map frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,49 @@
+import { useState, useRef, useEffect } from 'react'
+import './App.css'
+import mapboxgl from "mapbox-gl"
+
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiaGFydmV5dWJjIiwiYSI6ImNsaWRyenBieTB1dzgza3BmN2h3OTBmbW0ifQ.adlzsHRQg4Y4X0XJ8zLsCg';
+
+function App() {
+  const mapContainerRef = useRef<any>(null);
+  const [lng, setLng] = useState(5);
+  const [lat, setLat] = useState(34);
+  const [zoom, setZoom] = useState(1.5);
+
+  // Initialize map when component mounts
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: zoom
+    });
+
+    // Add navigation control (the +/- zoom buttons)
+    map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+
+    map.on('move', () => {
+      setLng(map.getCenter().lng);
+      setLat(map.getCenter().lat);
+      setZoom(map.getZoom());
+    });
+
+    // Clean up on unmount
+    return () => map.remove();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <div>
+      <div className='sidebarStyle'>
+        <div>
+          Longitude: {lng.toFixed(2)} | Latitude: {lat.toFixed(2)} | Zoom: {zoom.toFixed(2)}
+        </div>
+      </div>
+      <div className='map-container' ref={mapContainerRef} />
+    </div>
+  );
+};
+
+export default App
