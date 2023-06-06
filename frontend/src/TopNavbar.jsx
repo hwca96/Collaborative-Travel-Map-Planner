d3ecfa9837810234aea4bb29b17a6cccc166187d
Harvey Wu <hwca96@gmail.com>
2023-06-05 20:12:15 -0700

    changed name

--- frontend/src/TopNavbar.jsx
+++ frontend/src/TopNavbar.jsx
@@ -10,7 +10,7 @@ import {
 function TopNavbar() {
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="/">Collab Travel Planner</Navbar.Brand>
+      <Navbar.Brand href="/">Traveling Penguins</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
