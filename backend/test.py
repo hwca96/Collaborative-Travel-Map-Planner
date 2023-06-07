6f136770adfcc1b8585466378ace274aff14313a
JCurl08 <curl@student.ubc.ca>
2023-06-07 15:35:06 -0700

    Finished Flask Tutorial

--- backend/test.py
+++ backend/test.py
@@ -2,18 +2,5 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-data = [{"likes": 78, "name": "Joe", "views": 100000},
-        {"likes": 1000, "name": "How to make REST API", "views": 80000},
-        {"likes": 35, "name": "Tim", "views": 2000}]
-
-for i in range(len(data)):
-    response = requests.put(BASE + "video/" + str(i), data[i])
-    print(response.json())
-
-
-input()
-response = requests.delete(BASE + "video/0")
-print(response)
-input()
-response = requests.get(BASE + "video/2")
+response = requests.patch(BASE + "video/2", {})
 print(response.json())
